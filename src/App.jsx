f0e9a55295689e8d3d6dcb576134f9c86313e83b
Kaustav Sharma <sharmakaustav04@gmail.com>
2024-01-22 11:32:14 +0530

    First commit of working project

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import Hero from "./sections/Hero";
+import CustomerReviews from "./sections/CustomerReviews";
+import Footer from "./sections/Footer";
+import Nav from "./components/Nav";
+import PopularProducts from "./sections/PopularProducts";
+import Services from "./sections/Services";
+import Subscribe from "./sections/Subscribe";
+import SuperQuality from "./sections/SuperQuality";
+import SpecialOffer from "./sections/SpecialOffer";
+
+const App = () => (
+  <main className="relative">
+    <Nav />
+    <section className="x1:padding-l wide:padding-r padding-b">
+      <Hero />
+    </section>
+
+    <section className="padding">
+      <PopularProducts />
+    </section>
+
+    <section className="padding">
+      <SuperQuality />
+    </section>
+
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+
+    <section className="bg-pale-blue padding">
+      <CustomerReviews />
+    </section>
+
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
+
+  </main>
+);
+export default App;
